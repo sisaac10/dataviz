35ad458b985ddfb9bc0fbd8cf04e67278fcac84a
Sam <slall40@outlook.com>
2017-10-30 23:19:40 -0400

    Update hw.js

--- hw.js
+++ hw.js
@@ -1,9 +1,5 @@
 
 
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
 var theOneRing = {
 color:"yellow",
 material:"gold",
@@ -44,6 +40,11 @@ d3.csv("co2_income.csv", function(data) {
    .attr("class", "y axis")
    .attr("transform", "translate(" + (padding) +",0)")
    .call(yAxis);
+	
+   var xScale = d3.scale.linear()
+   .domain([0, xmax])
+   .range([padding, w - padding])
+   .nice();
     
 });
 
@@ -53,16 +54,6 @@ console.log(theOneRing.color);
 console.log(theOneRingOwners[1]);
 
 
-  if (hourNow > 18) {
-    greeting = 'Good Evening!';
-  } else if (hourNow > 12) {
-    greeting = 'Good Afternoon!';
-  } else if (hourNow > 0) {
-    greeting = 'Good Morning!';
-  } else {
-    greeting = 'Good Afternoon!';
-  }
-  document.write(greeting);
 
 
 
