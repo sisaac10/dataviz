b95b5efa1246479b5a9006ab76787b897fe249ef
Sam <slall40@outlook.com>
2017-10-30 22:57:01 -0400

    Update hw.js

--- hw.js
+++ hw.js
@@ -23,10 +23,13 @@ d3.csv("co2_income.csv", function(data) {
    var svg = d3.select("body").append("svg")
     .attr("width", 700);
    
-   svg.selectAll()
-   var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom");
+   svg.selectAll("circle")
+   .data(values)
+   .enter()
+   .append("circle")
+   .attr("cy", height/2)
+   .attr("r", 5)
+   .attr("cx", function(d) {return width * d;});
    
     console.log(svg.attr)
 
