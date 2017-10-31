adf937b50206eb869c9cece36bcedf70cfc66bbe
Sam <slall40@outlook.com>
2017-10-30 22:41:40 -0400

    Update hw.js

--- hw.js
+++ hw.js
@@ -14,13 +14,32 @@ var theOneRingOwners = {
 1:"Isildur"
 };
 
-var svg = d3.select("body").append("svg")
-    .attr("width", 700);
+
+
 
 
 
 d3.csv("co2_income.csv", function(data) {
-   
+   var svg = d3.select("body").append("svg")
+    .attr("width", 700);
+    
+    svg.selectAll()
+    var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+    
+    svg.append("g")
+   .attr("class", "x axis")
+   .attr("transform", "translate(0," + (h - padding) +")")
+   .call(xAxis);
+    var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+    svg.append("g")
+   .attr("class", "y axis")
+   .attr("transform", "translate(" + (padding) +",0)")
+   .call(yAxis);
 });
 
    
